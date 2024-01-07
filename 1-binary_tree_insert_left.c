a2bb80302cb65aab7d43b2f865808dee2bef7077
Mazionach <mazafesio25@gmail.com>
2024-01-07 14:18:21 -0800

    Fixed null parent handling

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,12 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *temp;
-	binary_tree_t *new = binary_tree_node(parent, value);
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
 
 	if (!new)
 		return (NULL);
