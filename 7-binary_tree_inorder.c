c6cd45077f28fc6a7dfb916d140d76218bc76dc8
Mazionach <mazafesio25@gmail.com>
2024-01-07 14:52:10 -0800

    Added inorder traversal function

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,19 @@
+
+
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_inorder - goes through a tree in inorder
+ * @tree: the tree
+ * @func: function to execute per node
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_inorder(tree->left);
+	func(tree);
+	binary_tree_inorder(tree->left);
+}
