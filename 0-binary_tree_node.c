fed4a4ac66cea23e91143d4799c336cc1344e9eb
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2024-01-07 12:52:30 -0300

    Tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,16 +7,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
+	binary_tree_t *new_node;
 
-	if (new_node == NULL)
-	{
+	new_node = malloc(sizeof(*parent));
+
+	if (!new_node)
 		return (NULL);
-	}
 
-	new_node->n = value;
 	new_node->parent = parent;
+	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
