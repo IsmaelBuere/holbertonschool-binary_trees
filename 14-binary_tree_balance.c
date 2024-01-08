08d81ecdaea66c9293ccd5d697a8496d1a6e596f
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2024-01-08 15:09:00 -0300

    Fixed function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,15 +6,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
-    return ((left_height > right_height ? left_height : right_height) + 1);
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 /**
@@ -25,13 +25,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	size_t left_heigh, right_height;
 
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    return ((int)(left_height - right_height));
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((int)(left_height - right_height));
 }
