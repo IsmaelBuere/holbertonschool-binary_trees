750b7149ddd5319e944c414f98976fb5c38724b0
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2024-01-08 15:11:44 -0300

    Fixed function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,7 +17,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return ((int)(left_height - right_height));
+	return (left_height - right_height);
 }
 /**
  * binary_tree_height - Computes the height of a binary tree
