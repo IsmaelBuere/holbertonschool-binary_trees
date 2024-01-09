7f2353726df5acc740ffcfc04a812535ca24ff47
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2024-01-08 22:31:53 -0300

    Fixed function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,48 +1,37 @@
 #include "binary_trees.h"
-
-size_t _binary_tree_height(const binary_tree_t *tree);
-
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: A pointer to the root node of the tree
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: A pointer to the root node of the binary tree.
  *
- * Return: Balance factor, 0 if tree is NULL
+ * Return: The balance factor (difference in height between the left and
+ *         right subtrees), or 0 if tree is NULL.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_balance, right_balance;
+	int left_height, right_height;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	left_balance = _binary_tree_height(tree->left);
-	right_balance = _binary_tree_height(tree->right);
 
-	return (left_balance - right_balance);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height - right_height);
 }
 /**
- * _binary_tree_height - Computes the height of a binary tree
- * @tree: A pointer to the root node of the tree
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the binary tree.
  *
- * Return: Height of the binary tree, 0 if tree is NULL
+ * Return: The height of the binary tree, or 0 if tree is NULL.
  */
-size_t _binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL)
-	{
+	if (!tree)
 		return (0);
-	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	left_height = binary_tree_height(tree->left) + 1;
-	right_height = binary_tree_height(tree->right) + 1;
-
-	if (left_height > right_height)
-	{
-		return (left_height);
-	}
-	else
-	{
-		return (right_height);
-	}
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
